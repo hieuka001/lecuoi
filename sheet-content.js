25d03f9429240a821886e806cd1dea7bdf51b5f3
Trung Hieu <trunghieubuikakaxx@gmail.com>
2026-04-02 21:58:10 +0700

    update

--- sheet-content.js
+++ sheet-content.js
@@ -1,8 +1,4 @@
 window.WEDDING_SHEET = {
-  // https://script.google.com/macros/s/AKfycbxvJsXF22m44o6yo-AJHel7oEI5bXVZuAcNn3Y1D6lkoeMzQkpKFJGNt2KGIh-I_K470A/exec
-  // Vi du: "https://script.google.com/macros/s/AKfycb.../exec"
-  webhookUrl: "",
-  // Optional token de xac thuc don gian
+  webhookUrl: "https://script.google.com/macros/s/AKfycbxvJsXF22m44o6yo-AJHel7oEI5bXVZuAcNn3Y1D6lkoeMzQkpKFJGNt2KGIh-I_K470A/exec",
   token: ""
 };
-
